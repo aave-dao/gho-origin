8e0c861bc11ca7c6c2920cf84b3f09b04e6aac8b
CheyenneAtapour <CheyenneAtapour@gmail.com>
2026-02-23 18:51:23 -0800

    chore: lint

--- src/deployments/projects/aave-v3-gho-facilitator/batches/GhoTokenBatch.sol
+++ src/deployments/projects/aave-v3-gho-facilitator/batches/GhoTokenBatch.sol
@@ -1,27 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import {GhoReportTypes} from "src/deployments/types/GhoReportTypes.sol";
-import {GhoTokenProcedure} from "src/deployments/contracts/procedures/GhoTokenProcedure.sol";
-import {GhoOracle} from "src/contracts/misc/GhoOracle.sol";
+import {GhoReportTypes} from 'src/deployments/types/GhoReportTypes.sol';
+import {GhoTokenProcedure} from 'src/deployments/contracts/procedures/GhoTokenProcedure.sol';
+import {GhoOracle} from 'src/contracts/misc/GhoOracle.sol';
 
 contract GhoTokenBatch is GhoTokenProcedure {
-    GhoReportTypes.GhoTokenReport _ghoTokenReport;
+  GhoReportTypes.GhoTokenReport _ghoTokenReport;
 
-    constructor(address deployer) {
-        address ghoToken = _deployGhoToken({tokenAdmin: deployer});
-        address upgradeableGhoToken = _deployUpgradeableGhoTokenProxy({
-            implementation: _deployUpgradeableGhoTokenImpl(), proxyAdmin: deployer, tokenAdmin: ghoToken
-        });
+  constructor(address deployer) {
+    address ghoToken = _deployGhoToken({tokenAdmin: deployer});
+    address upgradeableGhoToken = _deployUpgradeableGhoTokenProxy({
+      implementation: _deployUpgradeableGhoTokenImpl(),
+      proxyAdmin: deployer,
+      tokenAdmin: ghoToken
+    });
 
-        address ghoOracle = address(new GhoOracle());
+    address ghoOracle = address(new GhoOracle());
 
-        _ghoTokenReport = GhoReportTypes.GhoTokenReport({
-            ghoToken: ghoToken, upgradeableGhoToken: upgradeableGhoToken, ghoOracle: ghoOracle
-        });
-    }
+    _ghoTokenReport = GhoReportTypes.GhoTokenReport({
+      ghoToken: ghoToken,
+      upgradeableGhoToken: upgradeableGhoToken,
+      ghoOracle: ghoOracle
+    });
+  }
 
-    function getGhoTokenReport() public view returns (GhoReportTypes.GhoTokenReport memory) {
-        return _ghoTokenReport;
-    }
+  function getGhoTokenReport() public view returns (GhoReportTypes.GhoTokenReport memory) {
+    return _ghoTokenReport;
+  }
 }

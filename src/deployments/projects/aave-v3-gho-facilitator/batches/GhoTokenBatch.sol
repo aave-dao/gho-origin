ec46d0dc2cdafc5213459d95e290dfd37d3f99d9
CheyenneAtapour <CheyenneAtapour@gmail.com>
2026-02-25 15:13:42 -0800

    fix: gho token batch

--- src/deployments/projects/aave-v3-gho-facilitator/batches/GhoTokenBatch.sol
+++ src/deployments/projects/aave-v3-gho-facilitator/batches/GhoTokenBatch.sol
@@ -4,16 +4,20 @@ pragma solidity ^0.8.10;
 import {GhoReportTypes} from 'src/deployments/types/GhoReportTypes.sol';
 import {GhoTokenProcedure} from 'src/deployments/contracts/procedures/GhoTokenProcedure.sol';
 import {GhoOracle} from 'src/contracts/misc/GhoOracle.sol';
+import {ProxyAdmin} from 'src/contracts/dependencies/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol';
 
 contract GhoTokenBatch is GhoTokenProcedure {
   GhoReportTypes.GhoTokenReport _ghoTokenReport;
 
   constructor(address deployer) {
     address ghoToken = _deployGhoToken({tokenAdmin: deployer});
+    ProxyAdmin proxyAdmin = new ProxyAdmin();
+    proxyAdmin.transferOwnership(deployer);
+    require(proxyAdmin.owner() == deployer, 'PROXY_ADMIN_OWNER_MISMATCH');
     address upgradeableGhoToken = _deployUpgradeableGhoTokenProxy({
       implementation: _deployUpgradeableGhoTokenImpl(),
-      proxyAdmin: deployer,
-      tokenAdmin: ghoToken
+      proxyAdmin: address(proxyAdmin),
+      tokenAdmin: deployer
     });
 
     address ghoOracle = address(new GhoOracle());
